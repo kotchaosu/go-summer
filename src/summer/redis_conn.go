ac2ea358dda948258b0b056998bc9b0c98b05e95
Max Absent <grandguingol@o2.pl>
2013-09-04 23:44:45 +0200

    UNFINISHED: Copy/pasted connection pool

--- src/summer/redis_conn.go
+++ src/summer/redis_conn.go
@@ -5,6 +5,33 @@ package connection
 
 import redis
 
+var server string           // host:port of server
+var password string
+
+
+pool = &redis.Pool{
+	MaxIdle: 3,
+	IdleTimeout: 240 * time.Second,
+	Dial: func () (redis.Conn, error) {
+		c, err := redis.Dial("tcp", server)
+		
+		if err != nil {
+			return nil, err
+		}
+		
+		if err := c.Do("AUTH", password); err != nil {
+			c.Close()
+			return nil, err
+		}
+		return c, err
+	},
+	
+	TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		_, err := c.Do("PING")
+		return err
+	},
+}
+
 func main() {
 	//
 }
