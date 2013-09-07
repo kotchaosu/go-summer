c26a46a045ee82c01333a33c060a3da0d537d983
Max Absent <grandguingol@o2.pl>
2013-09-07 21:51:23 +0200

    Up and running parsing files and updating db

--- src/summer/dict_learn.go
+++ src/summer/dict_learn.go
@@ -1,35 +1,57 @@
-// package extracts and counts all words from learning set;
-// output (map[string]int) is saved in Redis DB
+// package extracts and counts all words from learning set
 package main
 
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"nlptk"
-	"redis"
+	"os"
+	"redis/redis"
+	"time"
 )
+
+var server = "127.0.0.1:6379" // host:port of server
+
+var pool = &redis.Pool{
+	MaxIdle:     3,
+	IdleTimeout: 240 * time.Second,
+	Dial: func() (redis.Conn, error) {
+		c, err := redis.Dial("tcp", server)
+
+		if err != nil {
+			return nil, err
+		}
+		return c, err
+	},
+
+	TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		_, err := c.Do("PING")
+		return err
+	},
+}
 
 const (
-	SETDIR = "/home/maxabsent/Documents/learning_set/full_texts/"
+	SETDIR       = "/home/maxabsent/Documents/learning_set/full_texts/"
+	DBDICTPREFIX = "#"
 )
 
-func WordCount(file_path string) map[string]int {
-	// extracts, trims from special signs and counts "bare" words in learning set
-	file, err := os.Open(file_path)
+// Extracts, trims from special signs and counts "bare" words in learning set.
+// Result dictionary is sent to channel.
+func WordCount(filename string, dict chan map[string]int) {
+	word_counter := make(map[string]int)
+	file, err := os.Open(SETDIR + filename)
 
 	if err != nil {
-		fmt.Println("Error reading file", file_path)
+		fmt.Println("Error reading file", filename)
 		os.Exit(1)
 	}
 
 	reader := bufio.NewReader(file)
-	word_counter := make(map[string]int)
 
 	for bpar, e := reader.ReadBytes('\n'); e == nil; bpar, e = reader.ReadBytes('\n') {
 		paragraph := nlptk.Paragraph{0, string(bpar)}
 		sentences := paragraph.GetParts()
-		
+
 		for _, sentence := range sentences {
 			s := nlptk.Sentence{0, sentence}
 			words := s.GetParts()
@@ -44,19 +66,26 @@ func WordCount(file_path string) map[string]int {
 			}
 		}
 	}
-	return word_counter
+	dict <- word_counter
 }
 
-func Store(dictionary map[string]int) {
-	// connect to db
-	connection := redis.newConnHdl(redis.DefaultSpec())
-	connection.connect()
-	// for every element in input dictionary
-	// check if word exists in db
-	//		1. create new row
-	//		2. update the old one
-	// close connection
-	connection.disconnect()
+func Store(dictionary chan map[string]int) {
+	connection := pool.Get()
+
+	for d := range dictionary {
+		for k, v := range d {
+
+			k = DBDICTPREFIX + k
+
+			_, err := connection.Do("EXISTS", k)
+			if err != nil {
+				connection.Do("SET", k, v) // create new entry
+			} else {
+				connection.Do("INCRBY", k, v) // update existing
+			}
+		}
+	}
+	connection.Close()
 }
 
 func main() {
@@ -73,25 +102,24 @@ func main() {
 	if err != nil {
 		fmt.Println("Error reading filenames from directory", SETDIR)
 		os.Exit(1)
-	}	
+	}
 
-	err := dir.Close()
+	err = dir.Close()
 
 	if err != nil {
 		fmt.Println("Error closing directory", SETDIR)
 		os.Exit(1)
 	}
 
-	// count words from all files
-	// and update database dictionary
-	c := make(chan map[string]int)
-	
+	dict := make(chan map[string]int)
+
 	go func() {
-		for _, v := range(files_slice) {
-			c <- WordCount(SETDIR + v)
+		for _, f := range files_slice {
+			WordCount(f, dict)
 		}
-		Store(<-c)
+		close(dict)
 	}()
+	Store(dict)
 
 	fmt.Println("Learning succeed!")
 }
