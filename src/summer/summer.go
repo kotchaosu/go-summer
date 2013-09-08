659502e569f44f865971adb2a9401ff6c847cea4
Max Absent <grandguingol@o2.pl>
2013-09-08 17:47:46 +0200

    Added algo-draft in comments

--- src/summer/summer.go
+++ src/summer/summer.go
@@ -1,1 +1,17 @@
 // summarization
+
+package main
+
+import (
+	// "os"
+	"fmt"
+	"dbconn"
+	"redis/redis"
+	)
+
+func main() {
+	// open and process file
+	// read model from db
+	// compute Viterbi
+	// print summarization
+}
