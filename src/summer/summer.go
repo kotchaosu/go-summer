cd646a7fa275f74d333fb3e832251ee41714f0f0
Max Absent <grandguingol@o2.pl>
2013-09-13 12:24:35 +0200

    Added functions for primitive inference and Viterbi algorithm

--- src/summer/summer.go
+++ src/summer/summer.go
@@ -5,7 +5,7 @@ package main
 import (
 	"os"
 	"bufio"
-	"hmm0"
+	"hmm1"
 	"fmt"
 	"nlptk"
 	"strings"
@@ -14,27 +14,96 @@ import (
 const(
 	FULL = "/home/maxabsent/Documents/learning_set/full_texts/"
 	SUMM = "/home/maxabsent/Documents/learning_set/summarizations/"
-	FILE = FULL + "text_6"
+	FILE = "/home/maxabsent/Documents/learning_set/evaluation/text_0"
 )
-	
 
-func Summarize(filename string, h *hmm0.HiddenMM) string {
-	full, err := os.Open(filename)
+
+func GetFileReader(filename string) *bufio.Reader {
+	file, err := os.Open(filename)
 
 	if err != nil {
 		fmt.Println("Error reading file", filename)
 		os.Exit(1)
 	}
 
-	summarization := make([]string, 0, 0)
+	return bufio.NewReader(file)
+}
+
+
+// Version for case #1 position in paragraph
+func CreateObservationSequence(filename string, length int) []int {
+	output := make([]int, length, length)
+	
+	sentence_number := 0
+	reader_full := GetFileReader(filename)
+
+	for bpar, e := reader_full.ReadBytes('\n'); e == nil; bpar, e = reader_full.ReadBytes('\n') {
+		var sentences []string
+		paragraph := nlptk.Paragraph{0, string(bpar)}
+
+		if sentences = paragraph.GetParts(); len(paragraph.Text) <= 1 || len(sentences) == 0 {
+			continue
+		}
+
+		for i := range sentences {
+			output[2 * sentence_number + 1] = i + 1
+		}
+
+		if sentence_number++; 2 * sentence_number + 1 >= length {
+			return output
+		}
+	}
+	fmt.Println("Created sequence:", output)
+	return output
+}
+
+// Prints sequence of states (appear, not appear) given by slice 
+func PrintSequence(filename string, sequence []int) string {
+	output := make([]string, 0, 0)
+	sentence_number := 0  // AKA states counter
+
+	reader_full := GetFileReader(filename)
+
+	for bpar, e := reader_full.ReadBytes('\n'); e == nil; bpar, e = reader_full.ReadBytes('\n') {
+		paragraph := nlptk.Paragraph{0, string(bpar)}
+
+		if len(paragraph.Text) <= 1 {
+			continue
+		}
+
+		sentences := paragraph.GetParts()
+
+		if len(sentences) == 0 {
+			continue
+		}
+
+		for _, s := range sentences {
+			if sequence[2 * sentence_number + 1] == 1 {
+				output = append(output, s)
+			}
+
+			sentence_number++
+			if 2 * sentence_number + 1 >= len(sequence) {
+				return strings.Join(output, ". ")
+			}
+		}
+	}
+	return strings.Join(output, ". ")
+}
+	
+
+// Main function for summarization
+func Summarize(filename string, h *hmm1.HiddenMM) string {
+	
 	sentence_number := 0  // AKA states counter
 	paragraph_number := 0
-	
 	current_state := 0
 
-	output_seq := make([]int, 0, 0)
+	summarization := make([]string, 0, 0)
+	output_seq := make([]int, hmm1.NSTATES)
 
-	reader_full := bufio.NewReader(full)
+	reader_full := GetFileReader(filename)
+
 	for bpar, e := reader_full.ReadBytes('\n'); e == nil; bpar, e = reader_full.ReadBytes('\n') {
 		paragraph := nlptk.Paragraph{paragraph_number, string(bpar)}
 
@@ -48,7 +117,7 @@ func Summarize(filename string, h *hmm0.HiddenMM) string {
 			continue
 		}
 
-		for _, s := range sentences {
+		for snum, s := range sentences {
 			// search highest probability through transition matrix
 			max_prob := 0.0
 			next_state := 0
@@ -63,31 +132,36 @@ func Summarize(filename string, h *hmm0.HiddenMM) string {
 
 			if current_state % 2 != 0 {
 				summarization = append(summarization, s)
-				output_seq = append(output_seq, 0)
+				output_seq[2 * sentence_number + 1] = snum + 1
+			} else {
+				output_seq[2 * sentence_number] = snum + 1
 			}
 
 			current_state = next_state
 
 			sentence_number++
 			// safety switch
-			if sentence_number == h.N / 2 {
-				return strings.Join(summarization, ".")
+			if 2 * sentence_number + 1 == h.N {
+				return strings.Join(summarization, ". ")
 			}
 		}
 	}
 	fmt.Println(output_seq)
-	return strings.Join(summarization, ".")
+	return strings.Join(summarization, ". ")
 }
 
 func main() {
-	hmm0.Educate(FULL, SUMM)
+	// hmm1.Educate(FULL, SUMM, 8, 0.01)
 	// read model from db
-	markovmodel := hmm0.Load()
+	markovmodel := hmm1.Load()
 	// print summarization
 	likelihood := 0.0
-	input_seq := make([]int, 30)
 
 	fmt.Println(Summarize(FILE, &markovmodel))
-	fmt.Println(input_seq)
-	fmt.Println(markovmodel.Viterbi(input_seq, &likelihood))
+
+	input_seq := CreateObservationSequence(FILE, hmm1.NSTATES)
+	vitout := markovmodel.Viterbi(input_seq, &likelihood)
+	
+	fmt.Println(vitout)
+	fmt.Println(PrintSequence(FILE, vitout))
 }
