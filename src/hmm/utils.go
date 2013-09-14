47860e3075e7e7601f733203a3f7e782a1988d12
Max Absent <grandguingol@o2.pl>
2013-09-14 14:47:10 +0200

    TODO

--- src/hmm/utils.go
+++ src/hmm/utils.go
@@ -3,18 +3,19 @@ package hmm
 // import (
 // 	"nlptk"
 // 	"math"
+// 	"dict"
 // 	)
 
-// Builds vectors of features for every sentence
-//
-// Feature vector:
-//	1. position within paragraph
-//	2. number of terms in sentence
-//	3. how likely the terms are given the baseline of terms
-//	4. how likely the terms are given the document terms
+// // Builds vectors of features for every sentence
+// //
+// // Feature vector:
+// //	1. position within paragraph
+// //	2. number of terms in sentence
+// //	3. how likely the terms are given the baseline of terms
+// //	4. how likely the terms are given the document terms
 // func ComputeFeatures(sentence nlptk.Sentence, document_dict map[string]int) []float64 {
 	
-// 	all_words := float64(GetWordCount(DBDICTPREFIX + "TOTAL"))
+// 	all_words := float64(dict.GetWordCount("TOTAL"))
 // 	all_doc_words := float64(document_dict["TOTAL"])	
 
 // 	words := sentence.GetParts()
@@ -25,12 +26,12 @@ package hmm
 // 	document_likelihood := 0.0
 
 // 	for _, word := range words {
-// 		base_count := float64(GetWordCount(word))
+// 		base_count := float64(dict.GetWordCount(word))
 // 		doc_count := float64(document_dict[word])
 
 // 		baseline_likelihood += math.Log10(base_count/all_words)
 // 		document_likelihood += math.Log10(doc_count/all_doc_words)
-// 	}http://www.youtube.com/watch?v=d4LdKapQaaE
+// 	}
 
 // 	features := [...]float64{
 // 		float64(number_of_sentence),
